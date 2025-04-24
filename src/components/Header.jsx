dd19226e80bb8b6fc22ece2b7be9a572fb7d47e1
JLTC3111 <dr.herbal2011@gmail.com>
2025-04-24 09:22:20 +0700

    Initial commit

---
+++ src/components/Header.jsx
@@ -0,0 +1,12 @@
+export function Header({ todos }) {
+    const incompleteTodos = todos.filter(todo => !todo.complete).length;
+    const taskOrTasks = incompleteTodos !== 1 ? 'Tasks' : 'Task';
+
+    return (
+        <header>
+            <h1 className="text-gradient">
+                You have <span className="font-bold text-4xl text-red-500">{incompleteTodos}</span> Incomplete {taskOrTasks}
+            </h1>
+        </header>
+    );
+}
