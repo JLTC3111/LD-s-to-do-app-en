ce9790a55ce64b1e1337f56f41443ae965f3d9d3
JLTC3111 <dr.herbal2011@gmail.com>
2025-04-29 01:00:23 +0700

    update

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -7,7 +7,7 @@ export function Header({ todos }) {
     <header>
         
             <h1 className="text-gradient">
-                You have <span className="font-bold text-4xl text-red-500">{incompleteTodos}</span> Incomplete {taskOrTasks}
+                <span className="font-bold text-4xl text-red-500">{incompleteTodos}</span> Incomplete {taskOrTasks}
             </h1>
         </header>
     );
