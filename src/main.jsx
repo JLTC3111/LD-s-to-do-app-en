486b650e52d8d13c5a2e42f73861d0d77a57dc77
JLTC3111 <dr.herbal2011@gmail.com>
2025-07-16 11:10:38 +0700

    update

--- src/main.jsx
+++ src/main.jsx
@@ -2,7 +2,7 @@ import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import App from './App.jsx'
 import './index.css'
-import './fanta.css'
+import './App.css'
 import './i18n/index.js'
 
 
